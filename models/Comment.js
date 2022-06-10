6133d979c5f188b2bd6462199a7064d80648075b
robertarthury <robertarthury@gmail.com>
2022-06-10 15:23:36 -0400

    validation

--- models/Comment.js
+++ models/Comment.js
@@ -1,16 +1,63 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
+const dateFormat = require('../utils/dateFormat');
+
+const ReplySchema = new Schema(
+  {
+    //set custom id to avoid confusion with parent comment _id
+    replyId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId()
+    },
+    replyBody: {
+      required: true,
+      type: String,
+      trim: true
+    },
+    writtenBy: {
+      type: String,
+      required: true
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: createdAtVal => dateFormat(createdAtVal)
+    }
+  },
+  {
+    toJSON: {
+      getters: true
+
+    }
+  }
+);
 
 const CommentSchema = new Schema({
   writtenBy: {
-    type: String
+    type: String,
+    required: ture
   },
   commentBody: {
-    type: String
+    type: String,
+    required: true
   },
   createdAt: {
     type: Date,
-    default: Date.now
-  }
+    default: Date.now,
+    get: createdAtVal => dateFormat(createdAtVal)
+  },
+  replies: [ReplySchema]
+},
+{
+  toJSON: {
+    virtuals: true,
+    getters: true
+  },
+  id: false
+ }
+);
+
+CommentSchema.virtual('replyCount').get(function() {
+  return this.replies.length;
 });
 
 const Comment = model('Comment', CommentSchema);
